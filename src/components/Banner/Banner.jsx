5424d3a65ff605d93aeaf64dbe38c4c49c9da52b
jerhum <jerhum@msn.com>
2025-07-04 09:08:59 +0200

    banner responsive

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ import "./banner.scss";
 
 function Banner(props) {
   return (
-    <div className="banner">
+    <div className="bannerContainer">
       <img src="./assets/banner-background.png" alt="banner background" />
       <div className="text">
       <p><div className="line"></div>Vous cherchez</p><span>un logement ?</span>
